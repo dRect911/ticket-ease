731990f474d67ab1d9fa036d7c284f1d2b5f61fd
dRect911 <n3m0ken@gmail.com>
2024-07-01 13:25:36 +0000

    misc: add home page backlink on dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,22 @@
-
-"use client"
-import withAuth from "@/lib/withAuth"
-
+"use client";
+import { Button } from "@/components/ui/button";
+import withAuth from "@/lib/withAuth";
+import Link from "next/link";
 
 function Dashboard() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<div className="flex flex-col justify-center mx-auto text-center" >
-  <p className='text-5xl font-bold'>This will be the dashboard</p>
-  <p className='text-3xl font-medium'>If you got here you are logged in</p>
-</div>
-      
+      <div className="flex flex-col justify-center mx-auto text-center">
+        <p className="text-5xl font-bold">This will be the dashboard</p>
+        <p className="text-3xl font-medium mb-8">
+          If you got here you are logged in
+        </p>
+        <Button asChild>
+          <Link href="/">Back to home page</Link>
+        </Button>
+      </div>
     </main>
-  )
+  );
 }
 
 export default withAuth(Dashboard);
