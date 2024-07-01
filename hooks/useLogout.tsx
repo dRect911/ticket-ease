6764207ec0009614fce6cc231bb1b9d952b67b3d
dRect911 <n3m0ken@gmail.com>
2024-07-01 12:57:41 +0000

    add: WIP | add useLogout hook

---
+++ hooks/useLogout.tsx
@@ -0,0 +1,30 @@
+import { supabase } from "@/lib/supabaseClient";
+// import { useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
+
+const useLogout = () => {
+  // const router = useRouter();
+  const { toast } = useToast();
+
+  const logout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Error signing out:", error.message);
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: error.message,
+      });
+    } else {
+      toast({
+        title: "Logout successful!",
+        description: "You will be rdirected to home page",
+      });
+      // router.push("/");
+    }
+  };
+
+  return logout;
+};
+
+export default useLogout;
