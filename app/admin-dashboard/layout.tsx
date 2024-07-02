fa0ee399cbb5b430da7d67fd8670238063708b8a
dRect911 <n3m0ken@gmail.com>
2024-07-02 13:29:32 +0000

    fix: minor | fix admin and driver dashboard layout names

--- app/admin-dashboard/layout.tsx
+++ app/admin-dashboard/layout.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from "next/navigation";
 
-export default function AuthLayout({
+export default function AdminDashboardLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
--- app/driver-dashboard/layout.tsx
+++ app/driver-dashboard/layout.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from "next/navigation";
 
-export default function AuthLayout({
+export default function DriverDashboardLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
