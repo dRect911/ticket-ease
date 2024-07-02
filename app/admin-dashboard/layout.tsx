9fff46a26971c9b3cf2427a0cabbc71a6c75bdbc
dRect911 <n3m0ken@gmail.com>
2024-07-02 15:28:26 +0000

    add: small update and role protection on admin dashboard

--- app/admin-dashboard/layout.tsx
+++ app/admin-dashboard/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useRoleProtection } from "@/hooks/useRoleProtection";
+import { supabase } from "@/utils/supabase/client";
 import { usePathname } from "next/navigation";
 
 export default function AdminDashboardLayout({
@@ -7,7 +9,21 @@ export default function AdminDashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const isAuthorized = useRoleProtection(supabase, "admin");
   const pathname = usePathname();
+
+  if (isAuthorized === null) {
+    return <div>Loading...</div>;
+  }
+
+
+  if (!isAuthorized) {
+    // further use toaster
+    return <div>
+      <p>Not Authorized</p>
+      <p>You will be redirected to home page</p>
+      </div>;
+  }
 
   return (
     <main className="h-screen w-screen flex justify-center items-center" >
