0cb071dc5f0ba723a2079740e2d35becc95d300f
dRect911 <n3m0ken@gmail.com>
2024-07-02 13:27:40 +0000

    ref: depreacate old utils module

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { supabase } from "../utils/supabase/client"
 
+// now deprecated
+// see @/utils/index.ts
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// see @/utils/supabase/client.ts
+export async function getUserData(){
+  try {
+    const { data: { user } } = await supabase.auth.getUser();
+    return user?.user_metadata; // Directly return user_metadata
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    // Handle the error here (e.g., return null or throw a new error)
+    return null; // Example: return null on error
+  }
 }
