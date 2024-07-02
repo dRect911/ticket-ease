25280e0eb3938d687a2938a772a2b063cda296e2
dRect911 <n3m0ken@gmail.com>
2024-07-02 13:27:05 +0000

    WIP: crafting the user dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+export default function AuthLayout({
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  return (
+    <main className="h-screen w-screen flex justify-center items-center" >
+      {children}
+    </main>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,41 @@
 "use client";
+import { getUserData } from "@/lib/utils";
+
 import { Button } from "@/components/ui/button";
 import withAuth from "@/lib/withAuth";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { UserMetadata } from "@supabase/supabase-js";
+import { getUser } from "@/utils/supabase/queries";
+import { supabase } from "@/utils/supabase/client";
+
 
 function Dashboard() {
+  const [userData, setUserData] = useState<UserMetadata | null | undefined>(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const data = await getUser(supabase);
+      setUserData(data?.user_metadata);
+    };
+
+    fetchUserData();
+  }, []);
+
+  
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col justify-center mx-auto text-center">
-        <p className="text-5xl font-bold">This will be the dashboard</p>
-        <p className="text-3xl font-medium mb-8">
-          If you got here you are logged in
-        </p>
-        <Button asChild>
-          <Link href="/">Back to home page</Link>
-        </Button>
+      {userData && (
+          <>
+            <p className="text-5xl font-bold p-4">Welcome {userData.first_name} </p>
+            <p className="text-3xl font-bold p-4">If you got here you are logged in</p>
+            <Button asChild>
+              <Link href="/">Back to home page</Link>
+            </Button>
+          </>
+        )}
       </div>
     </main>
   );
